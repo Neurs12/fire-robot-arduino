0eabf692e2a680dc5088dc4fb7d2eeaada41d226
Neurs <31346263+Neurs12@users.noreply.github.com>
2024-04-22 17:33:20 +0700

    Add actions to the robot, fix some silly issues

--- sketch.ino
+++ sketch.ino
@@ -1,14 +1,22 @@
 /**
-   Fire Robot Arduino
+  Fire Robot Arduino
 
-   Copyright (C) 2024, Neurs.
-   Released under the MIT License.
+  Copyright (C) 2024, Neurs.
+  Released under the MIT License.
+
+  https://github.com/Neurs12/fire-robot-arduino
 */
 
 // Might not be correct due to limited resources.
 
+
+// Time related variables
+// Delay time for each cycle in main loop.
+const int LOOP_DELAY = 100;
+const int SPRAY_TIME = 5000;
+
 // Enums to triggers different types of robot's functions
-enum FunctionState {
+enum TaskState {
   FIND_OBJECT, SPRAY_WATER, BACK_UP, STOP
 };
 
@@ -35,18 +43,19 @@ const byte SPRAY_WEAK = 12;
 
 const byte ARDUINO_LED = 13;
 
-void moveWheels(short leftWheel, short rightWheel) {
-  if (leftWheel != 0) {
-    analogWrite(leftWheel > 0 ? LEFT_WHEEL_FORWARD : LEFT_WHEEL_BACKWARD, abs(leftWheel));
-  } else {
-    analogWrite(LEFT_WHEEL_FORWARD, 0);
-    analogWrite(LEFT_WHEEL_BACKWARD, 0);
-  }
+void moveWheels(short rightWheel, short leftWheel) {
   if (rightWheel != 0) {
     analogWrite(rightWheel > 0 ? RIGHT_WHEEL_FORWARD : RIGHT_WHEEL_BACKWARD, abs(rightWheel));
   } else {
     analogWrite(RIGHT_WHEEL_FORWARD, 0);
     analogWrite(RIGHT_WHEEL_BACKWARD, 0);
+  }
+
+  if (leftWheel != 0) {
+    analogWrite(leftWheel > 0 ? LEFT_WHEEL_FORWARD : LEFT_WHEEL_BACKWARD, abs(leftWheel));
+  } else {
+    analogWrite(LEFT_WHEEL_FORWARD, 0);
+    analogWrite(LEFT_WHEEL_BACKWARD, 0);
   }
 }
 
@@ -55,9 +64,7 @@ void setupInputs() {
   pinMode(POWER_BUTTON, INPUT_PULLUP);
 
   // IR Sensors.
-  // Right sensor.
   pinMode(IR_RIGHT, INPUT_PULLUP);
-  // Left sensor.
   pinMode(IR_LEFT, INPUT_PULLUP);
 
   // Contact sensor.
@@ -86,21 +93,63 @@ void setupOutputs() {
 void setup() {
   // Define input ports.
   setupInputs();
-  
   // Define output ports.
   setupOutputs();
 }
 
 // Program's states.
 bool isRunning = true;
+TaskState currentTask = TaskState::FIND_OBJECT;
 
 void loop() {
   powerButtonHandler();
 
-  // Debug purpose
-  Serial.println("A");
+  if (isRunning) {
+    if (currentTask == TaskState::FIND_OBJECT) {
+      bool result = findObject();
+
+      // Object found, move to spray water mode.
+      if (result) {
+        currentTask = TaskState::SPRAY_WATER;
+      }
+    }
+
+    if (currentTask == TaskState::SPRAY_WATER) {
+      bool result = 
+
+      // Task finished, retreat from the object.
+      if (result) {
+        currentTask = TaskState::SPRAY_WATER;
+      }
+    }
+  }
+
   // Main loop.
-  delay(100);
+  delay(LOOP_DELAY);
+}
+
+void findObject() {
+  byte irRightRead = digitalRead(IR_RIGHT);
+  byte irLeftRead = digitalRead(IR_LEFT);
+  byte contactRead = digitalRead(FRONT_CONTACT);
+
+  if (irRightRead && irLeftRead) {
+    moveWheels(150, 150);
+  }
+  if (irRightRead && !irLeftRead) {
+    moveWheels(150, 0);
+  }
+  if (!irRightRead && irLeftRead) {
+    moveWheels(0, 150);
+  }
+
+  // Object found, return true for further instructions from mainloop.
+  if ((!irRightRead && !irLeftRead) || !contactRead) {
+    moveWheels(0, 0);
+    return true;
+  }
+
+  return false;
 }
 
 // Power button states.
